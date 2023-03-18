bf1ebfe656f8dadb1b1402ac0e9bcb3010742bd7
David Nkana <davidnkana74@gmail.com>
2023-03-18 02:25:52 -0700

    bug fix 12

--- client/script.js
+++ client/script.js
@@ -155,7 +155,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = ""
     loader(messageDiv)
 
-    const response = await fetch('// https://ai-cohort.onrender.com', {
+    const response = await fetch('https://ai-cohort.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
